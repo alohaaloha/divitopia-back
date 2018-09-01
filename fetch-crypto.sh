7c840a8cc1a9bad2ff000a93ba60f6051460e292
Radomir Marinkovic <radomirmarinkovic@Radomirs-MacBook-Pro.local>
2018-09-01 17:38:04 +0200

    #!/bin/sh

--- fetch-crypto.sh
+++ fetch-crypto.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 curl -H "Accept: application/json" -H "Content-Type: application/json" https://api.coinmarketcap.com/v2/ticker/?structure=array > public/info/crypto.json
--- fetch-fiat.sh
+++ fetch-fiat.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 curl -H "Accept: application/json" -H "Content-Type: application/json" https://openexchangerates.org/api/latest.json?app_id=APP_ID_HERE > public/info/fiat.json
